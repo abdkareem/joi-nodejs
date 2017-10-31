f68bc53a136373743f78b155503f20cfdcc64fca
Abdul Kareem <abd.khaja@gmail.com>
2017-10-30 22:28:35 -0700

    Code to handle field validation errors

--- errors.js
+++ errors.js
@@ -26,5 +26,196 @@ const errorKeys = {
     "er24": "ER-24",
     "er25": "ER-25",
     "er26": "ER-26",
-}
+    "er27": "ER-27",
+    "er28": "ER-28",
+    "er29": "ER-29",
+    "er30": "ER-30",
+    "er31": "ER-31",
+    "er32": "ER-32",
+    "er33": "ER-33",
+    "er34": "ER-34"
+};
+
+ERRORS.set(errorKeys.er01, {
+    code: 'ER-01',
+    detail: 'accountNo is not allowed to be empty'
+});
+
+ERRORS.set(errorKeys.er02, {
+    code: 'ER-02',
+    detail: 'accountNo must be a string'
+});
+
+ERRORS.set(errorKeys.er03, {
+    code: 'ER-03',
+    detail: 'routingNo is not allowed to be empty'
+});
+
+ERRORS.set(errorKeys.er04, {
+    code: 'ER-04',
+    detail: 'routingNo must be a string'
+});
+
+ERRORS.set(errorKeys.er05, {
+    code: 'ER-05',
+    detail: 'firstName is not allowed to be empty'
+});
+
+ERRORS.set(errorKeys.er06, {
+    code: 'ER-06',
+    detail: 'firstName must be a string'
+});
+
+ERRORS.set(errorKeys.er07, {
+    code: 'ER-07',
+    detail: 'lastName is not allowed to be empty'
+});
+
+ERRORS.set(errorKeys.er08, {
+    code: 'ER-08',
+    detail: 'lastName must be a string'
+});
+
+ERRORS.set(errorKeys.er09, {
+    code: 'ER-09',
+    detail: 'firstLine is not allowed to be empty'
+});
+
+ERRORS.set(errorKeys.er10, {
+    code: 'ER-10',
+    detail: 'firstLine must be a string'
+});
+
+ERRORS.set(errorKeys.er11, {
+    code: 'ER-11',
+    detail: 'secondLine must be a string'
+});
+
+ERRORS.set(errorKeys.er12, {
+    code: 'ER-12',
+    detail: 'city is not allowed to be empty'
+});
+
+ERRORS.set(errorKeys.er13, {
+    code: 'ER-13',
+    detail: 'city must be a string'
+});
+
+ERRORS.set(errorKeys.er14, {
+    code: 'ER-14',
+    detail: 'state is not allowed to be empty'
+});
+
+ERRORS.set(errorKeys.er15, {
+    code: 'ER-15',
+    detail: 'state must be a string'
+});
+
+ERRORS.set(errorKeys.er16, {
+    code: 'ER-16',
+    detail: 'zipCode is not allowed to be empty'
+});
+
+ERRORS.set(errorKeys.er17, {
+    code: 'ER-17',
+    detail: 'zipCode must be a string'
+});
+
+///////
+
+ERRORS.set(errorKeys.er18, {
+    code: 'ER-18',
+    detail: 'accountNo is not allowed to be empty'
+});
+
+ERRORS.set(errorKeys.er19, {
+    code: 'ER-19',
+    detail: 'accountNo must be a string'
+});
+
+ERRORS.set(errorKeys.er20, {
+    code: 'ER-20',
+    detail: 'routingNo is not allowed to be empty'
+});
+
+ERRORS.set(errorKeys.er21, {
+    code: 'ER-21',
+    detail: 'routingNo must be a string'
+});
+
+ERRORS.set(errorKeys.er22, {
+    code: 'ER-22',
+    detail: 'firstName is not allowed to be empty'
+});
+
+ERRORS.set(errorKeys.er23, {
+    code: 'ER-23',
+    detail: 'firstName must be a string'
+});
+
+ERRORS.set(errorKeys.er24, {
+    code: 'ER-24',
+    detail: 'lastName is not allowed to be empty'
+});
+
+ERRORS.set(errorKeys.er25, {
+    code: 'ER-25',
+    detail: 'lastName must be a string'
+});
+
+ERRORS.set(errorKeys.er26, {
+    code: 'ER-26',
+    detail: 'firstLine is not allowed to be empty'
+});
+
+ERRORS.set(errorKeys.er27, {
+    code: 'ER-27',
+    detail: 'firstLine must be a string'
+});
+
+ERRORS.set(errorKeys.er28, {
+    code: 'ER-28',
+    detail: 'secondLine must be a string'
+});
+
+ERRORS.set(errorKeys.er29, {
+    code: 'ER-29',
+    detail: 'city is not allowed to be empty'
+});
+
+ERRORS.set(errorKeys.er30, {
+    code: 'ER-30',
+    detail: 'city must be a string'
+});
+
+ERRORS.set(errorKeys.er31, {
+    code: 'ER-31',
+    detail: 'state is not allowed to be empty'
+});
+
+ERRORS.set(errorKeys.er32, {
+    code: 'ER-32',
+    detail: 'state must be a string'
+});
+
+ERRORS.set(errorKeys.er33, {
+    code: 'ER-33',
+    detail: 'zipCode is not allowed to be empty'
+});
+
+ERRORS.set(errorKeys.er34, {
+    code: 'ER-34',
+    detail: 'zipCode must be a string'
+});
+
+
+
+
+
+
+
+
+
+
+
 
